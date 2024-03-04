ae5f04744040eec615463fa7ccc7c1333f4eb7e7
IEEEMOSTAFA <skmostafa8888@gmail.com>
2024-03-04 14:06:09 +0600

    first commit

---
+++ max3.js
@@ -0,0 +1,21 @@
+const jim = 56;
+const tim = 89;
+const kim = 168;
+
+if(jim > tim  && jim > kim){
+    console.log('Jim is the ultimate boss ')
+}
+else if(tim > jim && tim > kim){
+    console.log("Tim is the boss")
+}
+else{
+    console.log('kim is the kardeshians boss')
+}
+
+// Home Work:
+function maxOfThree(num1,num2,num3){
+    // do it yourself
+}
+
+const max = Math.max(12,1,56)
+console.log('Max number is :: ', max);
